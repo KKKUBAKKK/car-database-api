ad8eedf4c88d9683ca44ceb70498e1ddf8ab86a7
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-01-09 10:56:55 +0100

    added some requests for employee

--- car-database-api/Controllers/Employee/CarsManagementController.cs
+++ car-database-api/Controllers/Employee/CarsManagementController.cs
@@ -127,4 +127,259 @@ public class CarsManagementController : ControllerBase
         var count = await _context.Cars.CountAsync();
         return Ok(count);
     }
+    
+    // Get x cars with id > lastId and filtered by category
+    [HttpGet("next/{lastId:int}/{type}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetNextCars(int lastId, CarType type)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id > lastId && c.type == type)
+            .OrderBy(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id < firstId and filtered by category
+    [HttpGet("previous/{firstId:int}/{type}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetPrevCars(int firstId, CarType type)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id < firstId && c.type == type)
+            .OrderByDescending(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id > lastId and filtered by category and availability
+    [HttpGet("next/available/{lastId:int}/{type}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetNextAvailableCars(int lastId, CarType type)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id > lastId && c.type == type && c.isAvailable)
+            .OrderBy(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id < firstId and filtered by category and availability
+    [HttpGet("previous/available/{firstId:int}/{type}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetPrevAvailableCars(int firstId, CarType type)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id < firstId && c.type == type && c.isAvailable)
+            .OrderByDescending(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id > lastId and filtered by category and availability
+    [HttpGet("next/unavailable/{lastId:int}/{type}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetNextUnavailableCars(int lastId, CarType type)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id > lastId && c.type == type && !c.isAvailable)
+            .OrderBy(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id < firstId and filtered by category and availability
+    [HttpGet("previous/unavailable/{firstId:int}/{type}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetPrevUnavailableCars(int firstId, CarType type)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id < firstId && c.type == type && !c.isAvailable)
+            .OrderByDescending(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id > lastId and filtered by availability
+    [HttpGet("next/available/{lastId:int}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetNextAvailableCars(int lastId)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id > lastId && c.isAvailable)
+            .OrderBy(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id < firstId and filtered by availability
+    [HttpGet("previous/available/{firstId:int}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetPrevAvailableCars(int firstId)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id < firstId && c.isAvailable)
+            .OrderByDescending(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id > lastId and filtered by availability
+    [HttpGet("next/unavailable/{lastId:int}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetNextUnavailableCars(int lastId)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id > lastId && !c.isAvailable)
+            .OrderBy(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with id < firstId and filtered by availability
+    [HttpGet("previous/unavailable/{firstId:int}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetPrevUnavailableCars(int firstId)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.id < firstId && !c.isAvailable)
+            .OrderByDescending(c => c.id)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get count of cars filtered by category
+    [HttpGet("count/{type}")]
+    public async Task<ActionResult<int>> GetCarsCount(CarType type)
+    {
+        var count = await _context.Cars
+            .Where(c => c.type == type)
+            .CountAsync();
+            
+        return Ok(count);
+    }
+    
+    // Get count of cars filtered by category and availability
+    [HttpGet("count/{type}/{availability}")]
+    public async Task<ActionResult<int>> GetCarsCount(CarType type, bool availability)
+    {
+        var count = await _context.Cars
+            .Where(c => c.type == type && c.isAvailable == availability)
+            .CountAsync();
+            
+        return Ok(count);
+    }
+    
+    // Get count of cars filtered by availability
+    [HttpGet("count/availability/{availability}")]
+    public async Task<ActionResult<int>> GetCarsCount(bool availability)
+    {
+        var count = await _context.Cars
+            .Where(c => c.isAvailable == availability)
+            .CountAsync();
+            
+        return Ok(count);
+    }
+    
+    // Get x cars with seearch term in name
+    [HttpGet("search/{term}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetCarsBySearchTerm(string term)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.producer.Contains(term) || c.model.Contains(term))
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with seearch term in name and filtered by category
+    [HttpGet("search/{term}/type/{type}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetCarsBySearchTerm(string term, CarType type)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.producer.Contains(term) || c.model.Contains(term) && c.type == type)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with seearch term in name and filtered by availability
+    [HttpGet("search/{term}/availability/{availability}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetCarsBySearchTerm(string term, bool availability)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.producer.Contains(term) || c.model.Contains(term) && c.isAvailable == availability)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get x cars with seearch term in name and filtered by category and availability
+    [HttpGet("search/{term}/{type}/{availability}")]
+    public async Task<ActionResult<IEnumerable<CarDto>>> GetCarsBySearchTerm(string term, CarType type, bool availability)
+    {
+        var cars = await _context.Cars
+            .Where(c => c.producer.Contains(term) || c.model.Contains(term) && c.type == type && c.isAvailable == availability)
+            .Take(10)
+            .ToListAsync();
+            
+        return Ok(_mapper.Map<IEnumerable<CarDto>>(cars));
+    }
+    
+    // Get count of cars with seearch term in name
+    [HttpGet("count/search/{term}")]
+    public async Task<ActionResult<int>> GetCarsCountBySearchTerm(string term)
+    {
+        var count = await _context.Cars
+            .Where(c => c.producer.Contains(term) || c.model.Contains(term))
+            .CountAsync();
+            
+        return Ok(count);
+    }
+    
+    // Get count of cars with seearch term in name and filtered by category
+    [HttpGet("count/search/{term}/type/{type}")]
+    public async Task<ActionResult<int>> GetCarsCountBySearchTerm(string term, CarType type)
+    {
+        var count = await _context.Cars
+            .Where(c => c.producer.Contains(term) || c.model.Contains(term) && c.type == type)
+            .CountAsync();
+            
+        return Ok(count);
+    }
+    
+    // Get count of cars with seearch term in name and filtered by availability
+    [HttpGet("count/search/{term}/availability/{availability}")]
+    public async Task<ActionResult<int>> GetCarsCountBySearchTerm(string term, bool availability)
+    {
+        var count = await _context.Cars
+            .Where(c => c.producer.Contains(term) || c.model.Contains(term) && c.isAvailable == availability)
+            .CountAsync();
+            
+        return Ok(count);
+    }
+    
+    // Get count of cars with seearch term in name and filtered by category and availability
+    [HttpGet("count/search/{term}/{type}/{availability}")]
+    public async Task<ActionResult<int>> GetCarsCountBySearchTerm(string term, CarType type, bool availability)
+    {
+        var count = await _context.Cars
+            .Where(c => c.producer.Contains(term) || c.model.Contains(term) && c.type == type && c.isAvailable == availability)
+            .CountAsync();
+            
+        return Ok(count);
+    }
 }
